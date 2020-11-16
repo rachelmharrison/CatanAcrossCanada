528af68f37cf0c420a96284c9236ba88cd6478f7
Rachel Harrison <rachelmharrison8@gmail.com>
2020-11-16 18:01:56 -0500

    number enchancements

--- src/components/NumberSpot.js
+++ src/components/NumberSpot.js
@@ -22,6 +22,10 @@ handleNumberClick = e => {
         this.setState(state => ({
           number: 2
         }));
+      else if(this.state.number == 6)
+        this.setState(state => ({
+          number: 8
+        }));
       else
         this.setState(state => ({
           number: this.state.number + 1
@@ -29,9 +33,25 @@ handleNumberClick = e => {
     }
 
 render() {
+ 
   return (
     <span className={this.classname} onClick={this.handleNumberClick}>
-       <p>{this.state.number}</p>
+        { (this.state.number == 6 || this.state.number == 8) &&
+          <p className='number red'>{this.state.number}</p>
+        }
+        { !(this.state.number == 6 || this.state.number == 8) &&
+          <p className='number'>{this.state.number}</p>
+        }
+       { this.state.number == 2 && <p className='dots'>.</p> }
+       { this.state.number == 3 && <p className='dots'>..</p> }
+       { this.state.number == 4 && <p className='dots'>...</p> }
+       { this.state.number == 5 && <p className='dots'>....</p> }
+       { this.state.number == 6 && <p className='dots red'>.....</p> }
+       { this.state.number == 8 && <p className='dots red'>.....</p> }
+       { this.state.number == 9 && <p className='dots'>....</p> }
+       { this.state.number == 10 && <p className='dots'>...</p> }
+       { this.state.number == 11 && <p className='dots'>..</p> }
+       { this.state.number == 12 && <p className='dots'>.</p> }
     </span>
 
   ) 
